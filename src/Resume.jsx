140e7fdbd1e9f1f161c94646e3436e63c2a569d8
surojeetGhosh <surojeetghosh89@gmail.com>
2022-10-07 21:16:38 +0530

    accordian done form remaining

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 export const Resume = () => {
     return(
         <div>
-            <Typography variant="h6" align={'center'}>
+            <Typography sx={{fontFamily: 'Merriweather, serif', fontSize: '1rem', mt: 2}} variant="h6" align={'center'}>
                 Final View
             </Typography>
         </div>
--- src/ResumeForm.jsx
+++ src/ResumeForm.jsx
@@ -1,10 +1,119 @@
 import { Typography } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Container } from "@mui/system";
+import { useState } from "react";
+
 export const ResumeForm = () => {
-    return(
-        <div>
-            <Typography variant="h6" align={'center'}>
-                Resume PlayGround
+  const [expanded, setExpanded] = useState();
+
+  const handleChange =
+    (panel) => (event, isExpanded) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+  return (
+    <div>
+      <Typography
+        sx={{ fontFamily: "Merriweather, serif", fontSize: "1rem", mt: 2 }}
+        variant="h6"
+        align={"center"}
+      >
+        Resume PlayGround
+      </Typography>
+      <Container maxWidth="lg" sx={{mt: 4}}>
+        <Accordion
+          expanded={expanded === "personal"}
+          onChange={handleChange("personal")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="personal-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             Personal Details
             </Typography>
-        </div>
-    );
-}
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={expanded === "brief"}
+          onChange={handleChange("brief")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="brief-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             Brief Description
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={expanded === "skills"}
+          onChange={handleChange("skills")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="skills-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             Skills
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={expanded === "education"}
+          onChange={handleChange("education")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="education-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             Education
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={expanded === "project"}
+          onChange={handleChange("project")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="project-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             Project/ Certification/ Internship
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          expanded={expanded === "links"}
+          onChange={handleChange("links")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            id="links-header"
+          >
+            <Typography sx={{ width: "33%", flexShrink: 0 }}>
+             web Links
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          </AccordionDetails>
+        </Accordion>
+      </Container>
+    </div>
+  );
+};
