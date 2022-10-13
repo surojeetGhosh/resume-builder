599af7857a80bdf99b4838f74302b856e2c3e461
surojeetGhosh <surojeetghosh89@gmail.com>
2022-10-13 22:45:08 +0530

    Added Profile pic functionality

--- src/components/formComponents/Personal.jsx
+++ src/components/formComponents/Personal.jsx
@@ -1,4 +1,4 @@
-import { TextField, Grid } from "@mui/material";
+import { TextField, Grid, Input, Avatar } from "@mui/material";
 import React from "react";
 const Personal = (props) => {
   const [formData, onDataChange] = React.useState({
@@ -15,20 +15,34 @@ const Personal = (props) => {
     email: false,
   });
 
+  const [image, onChangeImg] = React.useState(null); 
+
   function onFormChange(e) {
-    if (
-      e.target.id === "contact" &&
-      (e.target.value.length !== 10 || Number(e.target.value))
-    ) {
+    
+    if( e.target.id === "imageFile") {
+      var file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        onChangeImg(reader.result);
+      };
     }
     const obj = { ...formData, [e.target.id]: e.target.value };
-    console.log(obj);
     onDataChange(obj);
   }
 
   return (
     <Grid container justifyContent={"center"} columnSpacing={2} rowSpacing={2}>
-      <Grid item>
+      {(image !== null)? <Avatar alt="Profile-pic" src={image} />: null }
+      <Grid sx={{textAlign: "center"}} item lg={12} md={12}>
+        <Input
+          accept="image/*"
+          id="imageFile"
+          type="file"
+          onChange={onFormChange}
+        />
+      </Grid>
+      <Grid sx={{textAlign: "center"}} item lg={6}>
         <TextField
           id="fullname"
           onChange={onFormChange}
@@ -39,7 +53,7 @@ const Personal = (props) => {
           required
         />
       </Grid>
-      <Grid item>
+      <Grid sx={{textAlign: "center"}} item lg={6}>
         <TextField
           id="degree"
           value={formData.degree}
@@ -50,7 +64,7 @@ const Personal = (props) => {
           required
         />
       </Grid>
-      <Grid item>
+      <Grid sx={{textAlign: "center"}} item lg={6}>
         <TextField
           id="contact"
           value={formData.contact}
@@ -63,7 +77,7 @@ const Personal = (props) => {
           required
         />{" "}
       </Grid>
-      <Grid item>
+      <Grid sx={{textAlign: "center"}} item lg={6}>
         <TextField
           id="email"
           value={formData.email}
@@ -71,7 +85,7 @@ const Personal = (props) => {
           type={"email"}
           label="E-mail"
           variant="filled"
-          error={formError.contact}
+          error={formError.email}
           required
         />
       </Grid>
